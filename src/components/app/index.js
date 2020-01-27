40596e3c7a098dd8d8f7714a993c987104bff763
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-01-27 22:12:06 +0800

    charts initial

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../assets/logo.svg';
+import './style.css';
+
+import D3 from '../charts/d3'
 
 function App() {
   return (
@@ -10,14 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <D3 />
       </header>
     </div>
   );

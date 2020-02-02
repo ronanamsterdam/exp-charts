87cf631e7d7f6f638235eb1a75287bbd375ea97a
Ronan Amsterdam <romanfromrome.com@gmail.com>
2020-02-02 19:25:22 +0800

    styles and types upd

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../assets/logo.svg';
 import './style.css';
 
 import D3 from '../charts/d3'
@@ -9,12 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <D3 />
         <Victory />
+        <D3 />
       </header>
     </div>
   );
